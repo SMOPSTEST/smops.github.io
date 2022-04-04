3fd7d4f5b880bd120b0fa091533599d400932e65
Paolo Guardabasso <44571808+paolo-guba@users.noreply.github.com>
2022-04-04 14:16:15 +0200

    Started implementing dots but not working

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -10,14 +10,23 @@ function plusSlides(n, no) {
   showSlides(slideIndex[no-1] += n, no-1);
 }
 
+function currentSlide(n, no) {
+  showSlides(slideIndex[no-1] = n, no-1);
+}
+
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideClass[no]);
+  // var dots = document.getElementsByClassName("demo");
 
   if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" w3-white", "");
+  // }
   x[slideIndex[no]-1].style.display = "block";  
+  //dots[slideIndex[no]-1].className += " w3-white";
 }
